388495b3b30f804caa78e840c7c1392ccf594521
jhaezaerts <jorgo.haezaerts@gmail.com>
2023-03-25 20:33:48 +0100

    requirements

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def get_advice(description):
             {"role": "user", "content": "What are 5 key opportunities for AI that can be leveraged based on the "
                                         "following information: "
                                         + description
-                                        + ". Express enthusiasm about the description before listing the opportunities"},
+                                        + ". Express enthusiasm about the description before listing the opportunities"}
         ]
     )
     return st.write(response['choices'][0]['message']['content'])
@@ -30,7 +30,7 @@ def record(key):
                 recording_color="#cd0000",
                 neutral_color="#000000",
                 icon_name="fa-solid fa-microphone",
-                icon_size="3x",
+                icon_size="2xl",
                 key=key)
 
 
@@ -44,94 +44,69 @@ def whisper(bytes, responses, idx):
     return responses
 
 
-# Main page
-st.title("Uncovering AI in your business.")
-st.title("")
+def set_question(title, key, idx, responses):
+    st.title("")
+    st.subheader(title)
+    st.write("")
+    c1, c2, c3 = st.columns([1, 7, 2])
+    with c1:
+        audio_bytes = record(key)
+    with c2:
+        responses = whisper(audio_bytes, responses, idx)
+        if responses[idx]:
+            st.write(responses[idx])
+        else:
+            st.write("")
+    with c3:
+        if responses[idx]:
+            st.write(":heavy_check_mark:")
 
-st.write("**Hello there, my name is Bart (Business AI Readiness Tool). How would you like to interact with me?**")
-interaction = st.radio("", ("Text", "Speech"), label_visibility="collapsed")
-st.title("")
 
+def main():
+    st.title("Discover what AI can mean for your business.")
+    st.title("")
 
-if interaction == "Text":
-    st.subheader("Provide a clear description of your business.")
-    st.write('*line of work, key activities, desirable activities, time-consuming or error-prone processes, etc.*')
-    description = st.text_area(label="", label_visibility="collapsed", height=300)
-    if st.button("Generate advice"):
-        if description:
-            with st.spinner('Processing your response...'):
+    st.header("How would you like to interact with me?")
+    interaction = st.radio("", ("Text", "Speech"), label_visibility="collapsed")
+    st.title("")
+
+    if interaction == "Text":
+        st.header("Provide a clear description of your business.")
+        st.write('*line of work, key activities, desirable activities, time-consuming, repetitive or error-prone processes, etc.*')
+        description = st.text_area(label="",
+                                   placeholder="I am a car salesperson. Our company targets the luxurious market segment. We sell premier, high-quality cars in our physical stores, as well as online via our website. We mostly...",
+                                   label_visibility="collapsed",
+                                   height=300)
+        if st.button("Generate advice"):
+            if description:
+                with st.spinner('Processing your response...'):
+                    st.title("")
+                    get_advice(description)
                 st.title("")
+                st.success('You can always edit your text if you want to provide additional information.  \n  '
+                           'Good luck with your AI endeavours!')
+            else:
+                st.title("")
+                st.write("I need a description before I can generate advice.")
+
+    if interaction == "Speech":
+        responses = [None] * 5
+
+        set_question("What line of work are you in?", "q1", 0, responses)
+        set_question("What are the key operating activities of your business?", "q2", 1, responses)
+        set_question("Are there any processes that are time-consuming or error-prone?", "q3", 2, responses)
+        set_question("Are there any areas of your business that you think could benefit from AI?", "q4", 3, responses)
+        set_question("What kind of AI tools or technologies are you most interested in exploring?", "q5", 4, responses)
+
+        st.title("")
+        if None not in responses:
+            with st.spinner('Processing your responses...'):
+                description = ', '.join(responses)
                 get_advice(description)
             st.title("")
-            st.success('You can always edit your text if you want to provide additional information.  \n  '
+            st.success('You can always resubmit responses if you want to provide additional information.  \n  '
                        'Good luck with your AI endeavours!')
-        else:
-            st.title("")
-            st.write("I need a description before I can generate advice.")
 
-if interaction == "Speech":
-    responses = [None] * 5
 
-    # Question 1
-    c1, c2 = st.columns([1, 7])
-    with c1:
-        audio_bytes = record("q1")
-    with c2:
-        st.subheader("What line of work are you in?")
-        responses = whisper(audio_bytes, responses, 0)
-        if responses[0]:
-            st.write("*Response submitted :white_check_mark:*")
-
-    # Question 2
-    st.title("")
-    c1, c2 = st.columns([1, 7])
-    with c1:
-        audio_bytes = record("q2")
-    with c2:
-        st.subheader("What are the key operating activities of your business?")
-        responses = whisper(audio_bytes, responses, 1)
-        if responses[1]:
-            st.write("*Response submitted :white_check_mark:*")
-
-    # Question 3
-    st.title("")
-    c1, c2 = st.columns([1, 7])
-    with c1:
-        audio_bytes = record("q3")
-    with c2:
-        st.subheader("Are there any processes that are time-consuming or error-prone?")
-        responses = whisper(audio_bytes, responses, 2)
-        if responses[2]:
-            st.write("*Response submitted :white_check_mark:*")
-
-    # Question 4
-    st.title("")
-    c1, c2 = st.columns([1, 7])
-    with c1:
-        audio_bytes = record("q4")
-    with c2:
-        st.subheader("Are there any areas of your business that you think could benefit from AI?")
-        responses = whisper(audio_bytes, responses, 3)
-        if responses[3]:
-            st.write("*Response submitted :white_check_mark:*")
-
-    # Question 5
-    st.title("")
-    c1, c2 = st.columns([1, 7])
-    with c1:
-        audio_bytes = record("q5")
-    with c2:
-        st.subheader("What kind of AI tools or technologies are you most interested in exploring?")
-        responses = whisper(audio_bytes, responses, 4)
-        if responses[4]:
-            st.write("*Response submitted :white_check_mark:*")
-
-    st.title("")
-    if None not in responses:
-        with st.spinner('Processing your responses...'):
-            description = ', '.join(responses)
-            get_advice(description)
-        st.title("")
-        st.success('You can always resubmit responses if you want to provide additional information.  \n  '
-                   'Good luck with your AI endeavours!')
-
+if __name__ == "__main__":
+    main()
