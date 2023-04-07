0f8d7dccff5bd864af48b09fce525316237f1bc6
jhaezaerts <jorgo.haezaerts@gmail.com>
2023-04-07 13:39:42 +0200

    UI

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ def main():
     st.title("")
 
     if interaction == "Text":
-        c1, c2 = st.columns([18,2])
+        c1, c2 = st.columns([20,2])
         with c1:
             message = st.text_input(label="message",
                                     placeholder="Send a message...",
