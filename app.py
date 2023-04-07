d18a140c6eda847316162841e08fd77f37be6f13
jhaezaerts <jorgo.haezaerts@gmail.com>
2023-04-07 19:41:39 +0200

    UI

--- app.py
+++ app.py
@@ -220,9 +220,11 @@ def main():
             if st.session_state.index == 1:
                 bart1.write("**BART**")
                 q1.write(questions[0])
+                st.markdown("""---""")
                 user1.write("**" + username + "**")
                 r1.write(st.session_state.responses[0])
                 time.sleep(2)
+                st.markdown("""---""")
                 bart2.write("**BART**")
                 q2.write(questions[1])
             if st.session_state.index == 2:
