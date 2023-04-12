62a7a6506cde330a345e651d546d6df8e19479fc
jhaezaerts <jorgo.haezaerts@gmail.com>
2023-04-12 19:55:26 +0200

    UI

--- app.py
+++ app.py
@@ -243,7 +243,7 @@ def main():
                     st.session_state.responses[st.session_state.index] = stt["text"]
                     st.session_state.index += 1
                     st.session_state.input_message_key = str(random())
-                    stt = None
+                    stt["text"] = None
                     st.experimental_rerun()
 
             # Question processing
