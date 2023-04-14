290ac75c4766e82ac5df0e359befd55a518f476e
jhaezaerts <jorgo.haezaerts@gmail.com>
2023-04-14 22:41:56 +0200

    UI

--- app.py
+++ app.py
@@ -126,7 +126,7 @@ def main():
                                           label_visibility="collapsed",
                                           placeholder="Record your response...",
                                           disabled=True,
-                                          height=50,
+                                          height=30,
                                           key=st.session_state.input_message_key)
         # Record button
         with c2:
