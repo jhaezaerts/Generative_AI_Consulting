9d4a24179da8c9d91632e631c9f0c4bb2f429db0
jhaezaerts <jorgo.haezaerts@gmail.com>
2023-04-17 22:19:30 +0200

    UI

--- app.py
+++ app.py
@@ -201,7 +201,7 @@ def main():
 
 
         if st.session_state.index == 1:
-            message1.write(':red[' + '*' + str(st.session_state.responses[0]) + '*' + ']  \n')
+            message1.write(':red[' + '*' + str(st.session_state.responses[0]) + '*' + ']  \n ')
             message2.text_area(label=f"{username}",
                                label_visibility="collapsed",
                                placeholder="Record your response...",
