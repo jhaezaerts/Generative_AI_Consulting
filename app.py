8d1acc699123901f87fecd66c519ce420a21faf3
jhaezaerts <jorgo.haezaerts@gmail.com>
2023-05-01 18:00:42 +0200

    testing streamlit chat

--- app.py
+++ app.py
@@ -88,6 +88,8 @@ def main():
             for i in range(len(st.session_state.responses)):
                 chat(questions[i], avatar_style="bottts", seed="Buster")
                 chat(st.session_state.responses[i], is_user=True, avatar_style="initials", seed=username)
+                if i < len(questions):
+                    chat(questions[i+1], avatar_style="bottts", seed="Buster")
 
         c1, c2 = st.columns([9, 1])
         with c1:
